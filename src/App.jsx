1f80932ac54f3ceb77c172268941df6ca7491e1d
IghorAlves <ighoralc@gmail.com>
2023-05-18 21:54:50 -0300

    estruturando o navbar do project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //pages
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Project from "./components/pages/Projects";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
-        <h1>ola mundo</h1>
-      </div>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/project" element={<Project />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/pages/Projects.jsx
@@ -0,0 +1,9 @@
+function Project() {
+  return (
+    <div>
+      <h1>Project</h1>
+    </div>
+  );
+}
+
+export default Project;
