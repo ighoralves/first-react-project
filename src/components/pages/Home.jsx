3227aea4dada75a0cc27a63d7f6733b13173d779
IghorAlves <ighoralc@gmail.com>
2023-05-19 20:02:35 -0300

    finalizando a primeira pagina do projeto

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,19 @@
+import styles from "./Home.module.css";
+
+import savings from "../../img/savings.svg";
+
+import LinkButton from "../layout/LinkButton";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className={styles.home_container}>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a genrenciar seus projetos agora mesmo!</p>
+      <LinkButton to="/project" text="Criar Projeto" />
+      <img src={savings} alt="Costs" />
+    </section>
   );
 }
 
